e04ba4b95238789e4225950d5c559f7bd5db8f59
Santiago Meza <smmeza@unicesar.edu.co>
2026-08-14 13:02:57 -0500

    feat: crear clase Person como padre de Client

---
+++ src/main/java/com/mycompany/biblioteca/Person.java
@@ -0,0 +1,45 @@
+
+package com.mycompany.biblioteca;
+
+public class Person {
+    
+    private String id;
+    private String name;
+    private String phone;
+
+    public Person(String id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Phone: " + phone;
+    }
+    
+}
