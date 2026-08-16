cfcc0951b7154842f188cbb7e48ba8735b2f3e9f
Santiago Meza <smmeza@unicesar.edu.co>
2026-08-16 17:29:33 -0500

    feat: crear clase Material como padre de Book

---
+++ src/main/java/com/mycompany/biblioteca/Material.java
@@ -0,0 +1,42 @@
+package com.mycompany.biblioteca;
+
+public class Material {
+    private String code;
+    private String title;
+    private int publicationYear;
+
+    public Material(String code, String title, int publicationYear) {
+        this.code = code;
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Code: " + code + ", Title: " + title + ", Year: " + publicationYear;
+    }
+}
